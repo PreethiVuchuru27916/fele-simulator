b647417cc2a3270be184edbb6e7bd3a92ee64627
ramachandrapetla <ramachandra1996@gmail.com>
2023-02-01 21:21:52 -0600

    Initial Swagger Addition

--- fele-client-service/restful-api/feled.js
+++ fele-client-service/restful-api/feled.js
@@ -2,17 +2,17 @@ const express = require('express')
 const { couchdb, server } = require('../../conf/feleConf')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../../swagger.json')
 const { createNetwork, deleteNetwork } = require('../client-api/scripts/network')
 
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 const PORT = server.port
-
-const { json } = require('express')
 
 app.get('/createNetwork', async(req, res) => {
     let networkCreated = await createNetwork(req.query.networkConfigJSON, req.query.networkName)
