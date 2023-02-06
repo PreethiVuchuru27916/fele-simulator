8a6e625434b1251423b68a47704b5bca87613783
Shiva Medapati <shivaphaneendrareddymedapati@shivas-Air.attlocal.net>
2023-02-06 03:32:38 -0600

    Test cases for all commands

--- fele-client-service/cli/test/cli.spec.js
+++ fele-client-service/cli/test/cli.spec.js
@@ -4,6 +4,7 @@ const DEBUG = false; // change to true to see logs
 const {createNetworkCLI} = require('../scripts/network')
 const {deleteNetworkCLI} = require('../scripts/network')
 const {createChannel} = require('../scripts/channel')
+const {createChaincode} = require('../scripts/chaincode')
 const {interpreter} = require('../commands')
 // just a user defined function to help view logs
 const log = (value) => {
@@ -15,7 +16,7 @@ const log = (value) => {
 }
 jest.mock('../scripts/network')
 jest.mock('../scripts/channel')
-//jest.mock('../scripts/network')
+jest.mock('../scripts/chaincode')
 describe('user program test cases', () => {
   const userProgram = () => { // This is just a temp solution. This function has to be imported from fele.js so there is no redundency and also can be tested for all scenarios
     const program = new commander.Command();
@@ -36,8 +37,24 @@ describe('user program test cases', () => {
         '-nc',
         'state_felenet_gateway_v0_1.json'
       ]
+    
     interpreter.parse(commandArr, { from : 'user'});
     expect(createNetworkCLI).toHaveBeenCalledTimes(1)
+    
+  })
+  test('COMMANDER CMD TEST for creating a network', async () => {
+    //console.log({...network })
+    const commandArr = [
+        'network',
+        'create',
+        '-nn',
+        'gg',
+        '-nc',
+        '{"_id":"state.fele.felenet~root","fmt":"felenet","fmId":"root","includeLedger":false,"transactionAware":false,"blockAware":false,"timestamping":false,"walletUnaware":false}'  
+        
+      ]
+    interpreter.parse(commandArr, { from : 'user'});
+    expect(createNetworkCLI).toHaveBeenCalledTimes(2)
 
   })
 
@@ -66,6 +83,44 @@ describe('user program test cases', () => {
       ]
     interpreter.parse(commandArr, { from : 'user'});
     expect(createChannel).toHaveBeenCalledTimes(1)
+
+  })
+  
+ 
+ 
+  test('COMMANDER CMD TEST for creating a chaincode', async () => {
+    //console.log({...network })
+    const commandArr = [
+        'chaincode',
+        'register',
+        'create',
+        '-nn',
+        'artemis',
+        '-cn',
+        'nasa',
+        'ccn',
+        'assests'
+      ]
+    interpreter.parse(commandArr, { from : 'user'});
+    expect(createChaincode).toHaveBeenCalledTimes(1)
+
+  })
+
+  test('COMMANDER CMD TEST for creating a chaincode', async () => {
+    //console.log({...network })
+   const commandArr = [
+        'chaincode',
+        'register',
+        'create',
+        '--networkName',
+        'artemis',
+        '--channelName',
+        'nasa',
+        '--chaincodeName',
+        'assests'
+      ]
+    interpreter.parse(commandArr, { from : 'user'});
+    expect(createChaincode).toHaveBeenCalledTimes(2)
 
   })
 
