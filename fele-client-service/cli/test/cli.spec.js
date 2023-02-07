de722e5f085e7bae2b97df9d53d0eb229abe6050
Shiva Medapati <shivaphaneendrareddymedapati@shivas-MacBook-Air.local>
2023-02-07 00:52:31 -0600

    Test cases for all commands on CLI

--- fele-client-service/cli/test/cli.spec.js
+++ fele-client-service/cli/test/cli.spec.js
@@ -23,6 +23,10 @@ describe('user program test cases', () => {
     program
       .command('user')
       .option('--feleNetwork <feleNetwork>', 'Default feleNetwork name to be passed')
+      .option('--help', 'display help for command')
+      .option('--mspId <mspId>','mspId to be passed')
+      .option('--feleUser <feleUser>', 'feleUser to be passed')
+      .option('--password <password>', 'password to be passed')
       .action(() => {});
     return program;
   }
@@ -149,12 +153,58 @@ describe('user program test cases', () => {
     expect(program.args.length).toEqual(1);
   })
 
-  test('user command with network option', () => {  
+  test('user command with help option', () => {  
     const program = userProgram();
-    program.parse(['node', '../fele-client-service/cli/fele.js', 'user', '--feleNetwork', 'art']);
+    program.parse(['node', '../fele-client-service/cli/fele.js', 'user', '--help']);
     log(program.args); // just for debugging
-    expect(program.args).toEqual(['user', '--feleNetwork', 'art']);
+    expect(program.args).toEqual(['user', '--help']);
+    expect(program.args.length).toBeGreaterThan(0);
+    expect(program.args.length).toEqual(2);
+  })
+
+  test('user command with feleNetwork option', () => {  
+    const program = userProgram();
+    program.parse(['node', '../fele-client-service/cli/fele.js', 'user', '--feleNetwork', 'arg']);
+    log(program.args); // just for debugging
+    expect(program.args).toEqual(['user', '--feleNetwork','arg']);
     expect(program.args.length).toBeGreaterThan(0);
     expect(program.args.length).toEqual(3);
   })
+
+  test('user command with feleUser option', () => {  
+    const program = userProgram();
+    program.parse(['node', '../fele-client-service/cli/fele.js', 'user', '--feleUser', 'arg']);
+    log(program.args); // just for debugging
+    expect(program.args).toEqual(['user', '--feleUser','arg']);
+    expect(program.args.length).toBeGreaterThan(0);
+    expect(program.args.length).toEqual(3);
+  })
+
+  test('user command with password option', () => {  
+    const program = userProgram();
+    program.parse(['node', '../fele-client-service/cli/fele.js', 'user', '--password', 'arg']);
+    log(program.args); // just for debugging
+    expect(program.args).toEqual(['user', '--password','arg']);
+    expect(program.args.length).toBeGreaterThan(0);
+    expect(program.args.length).toEqual(3);
+  })
+
+  test('user command with mspId option', () => {  
+    const program = userProgram();
+    program.parse(['node', '../fele-client-service/cli/fele.js', 'user', '--mspId', 'arg']);
+    log(program.args); // just for debugging
+    expect(program.args).toEqual(['user', '--mspId','arg']);
+    expect(program.args.length).toBeGreaterThan(0);
+    expect(program.args.length).toEqual(3);
+  })
+
+  test('user command with all option', () => {  
+    const program = userProgram();
+    program.parse(['node', '../fele-client-service/cli/fele.js', 'user', '--mspId', 'nasa', '--feleUser', 'admin', '--password', '1234', '--feleNetwork', 'xyz']);
+    log(program.args); // just for debugging
+    expect(program.args).toEqual(['user', '--mspId', 'nasa', '--feleUser', 'admin', '--password', '1234', '--feleNetwork', 'xyz']);
+    expect(program.args.length).toBeGreaterThan(0);
+    expect(program.args.length).toEqual(9);
+  })
+
 })
