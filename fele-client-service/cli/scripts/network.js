2c0d33413aa940ca5dc714b026d94036ef67d526
Preethi Vuchuru <PreethiVuchuru@MacBook-Air-4.attlocal.net>
2023-02-11 15:22:47 -0600

    Authentication added and a network use command added

--- fele-client-service/cli/scripts/network.js
+++ fele-client-service/cli/scripts/network.js
@@ -1,28 +1,61 @@
-const { createDatabase, deleteDatabase, insertToDatabase } = require('../../utils/db')
+const {
+  createDatabase,
+  deleteDatabase,
+  insertToDatabase,
+} = require("../../utils/db");
 const path = require("path");
-const fs = require('fs');
-const { createNetwork, deleteNetwork } = require('../../client-api/scripts/network')
+const fs = require("fs");
+const {
+  createNetwork,
+  deleteNetwork,
+} = require("../../client-api/scripts/network");
 
-const USER_WORKSPACE = "../../../tmpworkspaceforuser/"
+const USER_WORKSPACE = "../../../tmpworkspaceforuser/";
+
+async function useNetworkCLI(username, localOrg, networkName) {
+  const { localUsers, felenetworks } = localOrg
+  let feleUser = {}
+  const localUserIndex = localUsers.findIndex((localUser) => (localUser.username === username));
+  if(localUserIndex != -1) {
+        const felenetworkIndex = felenetworks.findIndex((felenetwork) => felenetwork.felenetId === networkName);
+        if(felenetworkIndex != -1) {
+            const isMappingPresentForUser = felenetworks[felenetworkIndex].mappings.findIndex((mapping) => mapping.from === username);
+            if(isMappingPresentForUser != -1) {
+                feleUser.user = felenetworks[felenetworkIndex].mappings[isMappingPresentForUser].to
+                const feleuserIndex = felenetworks[felenetworkIndex].feleusers.findIndex((feleuser) => (feleuser.feleuserId === feleUser.user))
+                if(feleuserIndex != -1) {
+                  feleUser.wallet = felenetworks[felenetworkIndex].feleusers[feleuserIndex].walletId
+                }
+                return feleUser
+            }
+            else console.log("You are not authorized to this network")
+        }else{
+            console.log("Network does not exist")
+        }
+    }
+    return feleUser
+}
 
 async function createNetworkCLI(networkConfig, networkName) {
-    
-    if(networkConfig.includes(".json")) { //When networkConfig is a file
-        const fileName = USER_WORKSPACE+networkConfig;
-        networkConfig = require(fileName); //Get contents of file as object
-        networkConfig = JSON.stringify(networkConfig) //Convert object to string
-    } else {
-        networkConfig = networkConfig.replace(/\\/g,'')
-    }
-    console.log("after removing chars"+networkConfig)
-    createNetwork(networkConfig, networkName)
-} 
+  
+  if (networkConfig.includes(".json")) {
+    //When networkConfig is a file
+    const fileName = USER_WORKSPACE + networkConfig;
+    networkConfig = require(fileName); //Get contents of file as object
+    networkConfig = JSON.stringify(networkConfig); //Convert object to string
+  } else {
+    networkConfig = networkConfig.replace(/\\/g, "");
+  }
+  console.log("after removing chars" + networkConfig);
+  createNetwork(networkConfig, networkName);
+}
 
 async function deleteNetworkCLI(networkName) {
-    deleteNetwork(networkName)
+  deleteNetwork(networkName);
 }
 
 module.exports = {
-    createNetworkCLI,
-    deleteNetworkCLI
-}
+  createNetworkCLI,
+  deleteNetworkCLI,
+  useNetworkCLI
+};
---
+++ fele-client-service/utils/auth.js
@@ -0,0 +1,24 @@
+const authenticateUser = (username, password, organizationName, localOrg) => {
+  let localUser = { "authenticated" : false }
+  const { organization, localUsers } = localOrg;
+
+  if (organization === organizationName) {
+    
+    const localUserIndex = localUsers.findIndex(
+      localUser => localUser.username === username && localUser.password === password
+    );
+    //Found LocalUser
+    if (localUserIndex != -1) {
+      localUser.username = username;
+      localUser.password = password;
+      localUser.organization = organizationName;
+      localUser.authenticated = true;
+      return localUser;
+    }
+  }
+  return localUser;
+};
+
+module.exports = {
+  authenticateUser,
+};
