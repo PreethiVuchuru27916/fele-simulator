9a7333b4c35db1e5c3b16f805f4f285261d37fbd
Thomas639 <48922827+Thomas639@users.noreply.github.com>
2023-02-13 08:03:00 -0600

    Add files via upload

--- fele-client-service/cli/scripts/channel.js
+++ fele-client-service/cli/scripts/channel.js
@@ -1,22 +1,41 @@
-const fs = require('fs');
+const { checkIfNetworkExists, insertToDatabase } = require('../../utils/db')
 const path = require("path");
+const fs = require('fs');
+const USER_WORKSPACE = "../../../tmpworkspaceforuser/"
 
-function createChannel(networkName, channelName) {
-  var dir = "../../../chaincode/"+networkName+"/"+channelName
-  dir = path.join(__dirname, dir);
-  try {
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir)
-        console.log('Directory created.')
-      } else {
-        console.log('Directory already exists.')
-      }
-  } catch (err) {
-    console.log(err)
-  }
-}
+function createChannel(networkName, channelName, channelConfig)
+{
+  const database = "fele__"+networkName;
+  try{
+    const dbStatus = checkIfNetworkExists(database)
+    if(dbStatus){
+        console.log("Network "+ database+" exists")
+        if(channelConfig.includes(".json")) {
+          const fileName = USER_WORKSPACE+channelConfig;
+          channelConfig = require(fileName);
+          channelConfig = JSON.stringify(channelConfig)
+        } else {
+          channelConfig = channelConfig.replace(/\\/g,'')
+        }
+        const cconfig = insertToDatabase(database, JSON.parse(channelConfig));
+        if (cconfig){
+          var dir = "../../../chaincode/"+networkName+"/"+ channelName
+          dir = path.join(__dirname, dir)
+          if (!fs.existsSync(dir)) {
+              fs.mkdirSync(dir)
+              console.log('Directory created.')
+              }
+          else {
+              console.log('Directory already exists.')
+              }
+          }
+        }
+     }catch(error) {
+         return false
+     }
+     return false
+ }
 
-module.exports = {
-    createChannel
+ module.exports = {
+  createChannel
 }
- 
