7894ad8f6b5c69d74fb7b2512be0c8bda8532307
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2023-02-13 14:40:55 -0600

    Updated json schema validator name

--- fele-client-service/utils/schema-validator.js
+++ fele-client-service/utils/schema-validator.js
@@ -2,18 +2,18 @@ const ZSchema = require("z-schema");
 const validator = new ZSchema();
 
 // simple javascript function that returns a object based on validation of json schema
-const validateSchema = async (jsonSchema = {}, jsonData = {}) => {
+const validateJSON = async (jsonSchema = {}, jsonData = {}) => {
   try {
-    const jsonIsValid = validator.validate(jsonData, jsonSchema); // This is used to validate the schema. Returns err object or valid object
+    const jsonIsValid = validator.validate(jsonData, jsonSchema); // This is used to validate the schema. Returns true or false based on validity
     const errors = validator.getLastErrors();
 
     if (jsonIsValid) return { errors: false }; // return object that JSON data is valid
-    if (errors && errors.length) return { errors: true, message: JSON.stringify(errors.map((error) => { return { "message": error.message }})) };  // return the object of errors
+    if (errors && errors.length) return { errors: true, message: errors.map((error) => error.message) };  // return the object of errors
   } catch (error) {
     return { errors: true, message: error }
   }
 }
 
 module.exports = {
-  validateSchema
+  validateJSON
 }
