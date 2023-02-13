12fb04689cc8e50ea479f5864ff7dd38f5e6eff3
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2023-02-13 16:21:13 -0600

    Updated zschema to ajv for json validation

--- fele-client-service/utils/schema-validator.js
+++ fele-client-service/utils/schema-validator.js
@@ -1,16 +1,24 @@
-const ZSchema = require("z-schema");
-const validator = new ZSchema();
+const Ajv = require("ajv");
+const ajv = new Ajv({ strict: false, allErrors: true });
 
-// simple javascript function that returns a object based on validation of json schema
-const validateJSON = async (jsonSchema = {}, jsonData = {}) => {
-  try {
-    const jsonIsValid = validator.validate(jsonData, jsonSchema); // This is used to validate the schema. Returns true or false based on validity
-    const errors = validator.getLastErrors();
+const parseErrors = (validationErrors = [])=> {
+  let errors = [];
+  validationErrors.forEach(error => {
+    errors.push({
+      param: error.params["missingProperty"],
+      key: error.keyword,
+      message: error.message,
+    });
+  });
+  return errors;
+}
 
-    if (jsonIsValid) return { errors: false }; // return object that JSON data is valid
-    if (errors && errors.length) return { errors: true, message: errors.map((error) => error.message) };  // return the object of errors
-  } catch (error) {
-    return { errors: true, message: error }
+const validateJSON = (jsonSchema = {}, jsonData = {}) => {
+  const validator = ajv.compile(jsonSchema);
+  const isValid = validator(jsonData);
+  if (!isValid) {
+     const errors = parseErrors(validator.errors);
+     throw errors;
   }
 }
 
