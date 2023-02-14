717f7f7f36f73e2a41fdf4f1c8348caeed9ce35b
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2023-02-13 21:35:39 -0600

    Added json test files

--- fele-client-service/utils/schema-validator.js
+++ fele-client-service/utils/schema-validator.js
@@ -1,11 +1,14 @@
 const Ajv = require("ajv");
 const ajv = new Ajv({ strict: false, allErrors: true });
 
+const jsonSchema = require("../../tmpworkspaceforuser/test-data/testSchema.json");
+const jsonData = require("../../tmpworkspaceforuser/test-data/testSchemaData.json");
+
 const parseErrors = (validationErrors = [])=> {
   let errors = [];
   validationErrors.forEach(error => {
     errors.push({
-      param: error.params["missingProperty"],
+      param: JSON.stringify(error.params),
       key: error.keyword,
       message: error.message,
     });
@@ -17,10 +20,13 @@ const validateJSON = (jsonSchema = {}, jsonData = {}) => {
   const validator = ajv.compile(jsonSchema);
   const isValid = validator(jsonData);
   if (!isValid) {
-     const errors = parseErrors(validator.errors);
-     throw errors;
+      const errors = parseErrors(validator.errors);
+      return { valid: isValid, errors };
   }
+  return { valid: isValid };
 }
+
+console.log(validateJSON(jsonSchema, jsonData));
 
 module.exports = {
   validateJSON
