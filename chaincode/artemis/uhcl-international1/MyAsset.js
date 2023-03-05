9fabfc08b06fe6e8b11172842f601c92fc65efe0
Preethi Vuchuru <PreethiVuchuru@preethivuchuru.attlocal.net>
2023-03-04 20:07:03 -0600

    added changes

--- chaincode/artemis/uhcl-international1/MyAsset.js
+++ chaincode/artemis/uhcl-international1/MyAsset.js
@@ -4,17 +4,22 @@ class MyAsset {
     async readAsset(key) {
         const result = await new SmartContract().getState(key);
         console.log(result)
+        return result;
     } 
     async putAsset(key, value) {
         const result = await new SmartContract().putState(key, value);
         console.log(result)
+        return result;
     } 
 }
 
 const testFunc = async() => {
     const asset = new MyAsset();
-    const addedData = await asset.putAsset('123', { "data": "hello" });
-    console.log(addedData);
+    //const addedData = await asset.putAsset('123', { "data": "hello" });
+    const readData = await asset.readAsset('123');
+    //console.log(addedData);
+    console.log(readData);
 }
+
 
 testFunc();
