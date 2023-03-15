089c308ce2b087c46ff1447f92d196d007f2702d
Thomas639 <thomasjos639@gmail.com>
2023-03-14 22:25:47 -0500

    Chaincode changes

--- fele-client-service/cli/scripts/chaincode.js
+++ fele-client-service/cli/scripts/chaincode.js
@@ -1,33 +1,20 @@
-const fs = require('fs');
-const path = require("path");
 const logger = require('../../utils/logger');
-const {NETWORK_BASEPATH,USER_WORKSPACE} = require('../../../globals')
+const { createChaincode, invokeChaincode} = require('../../client-api/scripts/chaincode')
 
-function createChaincodeCLI(networkName, channelName, chaincodeName) {
+async function createChaincodeCLI(networkName, channelName, chaincodeName) {
 
-  const chaincodePath = path.join(NETWORK_BASEPATH, networkName, channelName, chaincodeName)
   try {
-      if (!fs.existsSync(chaincodePath)) {
-        fs.mkdirSync(chaincodePath)
-        console.log('Directory created.')
-      } else {
-        console.log('Directory already exists.')
-      }
-      fs.copyFileSync(USER_WORKSPACE+'/'+chaincodeName+'.js',NETWORK_BASEPATH+'/'+networkName+'/'+channelName+'/'+chaincodeName+'/'+chaincodeName+'.js')
-  } catch (err) {
-    console.log(err)
+    await createChaincode(networkName, channelName, chaincodeName);
+  } 
+  catch (err) {
+    logger.error(err);
   }
 }
 
 async function invokeChaincodeCLI(networkName, channelName, chaincodeName, argumentJSON) {
 
-  const chcode = require(NETWORK_BASEPATH+'/'+networkName+'/'+channelName+'/'+chaincodeName+'/'+chaincodeName);
-  const chClass = new chcode[chaincodeName]();
-  const functionToCall = argumentJSON.Args[0];
-  const functionArgs = argumentJSON.Args.slice(1);
   try{
-    const result = await chClass[functionToCall](...functionArgs)
-    console.log(result);
+    await invokeChaincode(networkName, channelName, chaincodeName, argumentJSON);
   }
   catch(err){
     logger.error(err);
