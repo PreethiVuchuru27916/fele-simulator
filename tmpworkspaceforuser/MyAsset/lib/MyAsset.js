4181bf6fa0daff8d06ea88039adbdfaedb40875f
Preethi Vuchuru <PreethiVuchuru@preethivuchuru.attlocal.net>
2023-03-17 23:41:22 -0500

    Replaced put asset with create asset

--- tmpworkspaceforuser/MyAsset/lib/MyAsset.js
+++ tmpworkspaceforuser/MyAsset/lib/MyAsset.js
@@ -1,13 +1,15 @@
 const { SmartContract } = require('../../../../../fele-smart-contract/SmartContract');
 
 class MyAsset extends SmartContract {
-    async readAsset(key) {
-        const result = await SmartContract.getState(key);
+
+    async createAsset(key, value) {
+        const result = await SmartContract.putState(key, value);
         console.log(result)
         return result;
     } 
-    async putAsset(key, value) {
-        const result = await SmartContract.putState(key, value);
+
+    async readAsset(key) {
+        const result = await SmartContract.getState(key);
         console.log(result)
         return result;
     } 
